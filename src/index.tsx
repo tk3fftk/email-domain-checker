6ef41e6c83f943a3e5ee84f2b4ea72653ffd2928
Hiroki Takatsuka <3445553+tk3fftk@users.noreply.github.com>
2024-07-14 17:12:45 +0900

    Form (#1)
    * implement min
    
    * handle empty
    
    * handle domain only text
    
    * fix text

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,83 @@ import { Hono } from "hono";
 import { renderer } from "./renderer";
 
 const app = new Hono();
+const emailBlocklistUrl =
+  "https://raw.githubusercontent.com/disposable-email-domains/disposable-email-domains/master/disposable_email_blocklist.conf";
+const emailAllowlistUrl =
+  "https://raw.githubusercontent.com/disposable-email-domains/disposable-email-domains/master/allowlist.conf";
+
+const Result = (result: string) => {
+  return (
+    <div>
+      <h1>Email Address/Domain Verification Result</h1>
+      <div>{result}</div>
+    </div>
+  );
+};
+
+const Form = (input: string) => {
+  return (
+    <form action="">
+      <label for="email">Email Address or Domain </label>
+      <input type="text" id="email" name="email" value={input} />
+      <button type="submit">Check</button>
+    </form>
+  );
+};
+
+function validateEmailOrDomain(text: string) {
+  const regex = /^([^\s@]+@)?[^\s@]+\.[^\s@]+$/;
+  return regex.test(text);
+}
+
+async function verifyEmailOrDomain(text: string) {
+  const blocklist: string[] = [];
+  try {
+    const response = await fetch(emailBlocklistUrl);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const responseText = (await response.text()).split("\n");
+    blocklist.push(...responseText);
+  } catch (error: any) {
+    console.error(`fetch error: ${error.message}`);
+  }
+  const domain = text.split("@")[1] || text;
+  return blocklist.includes(domain);
+}
 
 app.use(renderer);
 
-app.get("/", (c) => {
-  return c.render(<h1>Hello, Cloudflare Pages!!</h1>);
+app.get("/", async (c) => {
+  const renderHtml = [];
+  let email = c.req.query("email") || "";
+  renderHtml.push(Form(email));
+  if (email === "") {
+    return c.render(<>{renderHtml}</>);
+  }
+  if (!validateEmailOrDomain(email)) {
+    email = `Invalid Email Address or Domain: ${email}`;
+    renderHtml.push(Result(email));
+    return c.render(<>{renderHtml}</>);
+  }
+  const verifyResult = await verifyEmailOrDomain(email);
+  if (verifyResult) {
+    renderHtml.push(
+      Result(
+        `💣️捨てアドとして利用される疑いがあるEmailドメインです / 
+        The domain of this Email adress is suspected as a disposable email address: ${email}`
+      )
+    );
+  } else {
+    renderHtml.push(
+      Result(
+        `捨てアド疑いのリストにはありませんが、下記の情報も参考にしてください / 
+        The domain of this Email address is not in suspected list but be careful: ${email}`
+      )
+    );
+  }
+  return c.render(<>{renderHtml}</>);
 });
 
 export default app;
